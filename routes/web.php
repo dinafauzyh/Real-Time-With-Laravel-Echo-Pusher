251f74d72ee16d3b0c16ffacf53fc4a923188a5c
dinafauzyh <dfauziyah18@gmail.com>
2022-02-02 10:03:38 +0700

    6 Start chatting

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\HomeController;
 Route::middleware('auth')->group(function () {
     Route::get('/', HomeController::class);
     Route::get('/chat/{user:username}', [ChatController::class, 'show'])->name('chats.show');
+    Route::post('/chat/{user:username}', [ChatController::class, 'store'])->name('chats.store');
 });
 
 require __DIR__ . '/auth.php';
